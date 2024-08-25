62b517a278a020563aea837cd5223692bbcd0072
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-25 14:12:19 +0200

    ✨ FashionPage : Banner > add bannerLogo

--- src/components/pages/fashion/FashionPage.tsx
+++ src/components/pages/fashion/FashionPage.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import HomePage from "./homePage/HomePage";
+import BannerLogo from "./banner/BannerLogo";
 
 export default function FashionPage() {
   return (
     <FashionPageStyled>
       <HomePage/>
+      <BannerLogo/>
     </FashionPageStyled>
   );
 }
